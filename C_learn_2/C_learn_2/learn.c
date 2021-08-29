ac066548678a18a56404025c2f3a52187cc8df35
ChancWys <458490240@qq.com>
2021-08-29 14:48:26 +0800

    proj1  and  2   git test

---
+++ C_learn_2/C_learn_2/learn.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main()
+//{
+//	//const——常属性
+//	//const修饰的常变量。（具有常属性性质的变量）
+//	const int num = 4;//3, 100, 1.12等都是字面常量
+//	print("%d\n", num);
+//	num = 8;
+//	print("%d\n", num);
+//	return 0;
+//}
+
+//int main()
+//{
+//	const int n = 10;
+//	int arr[n] = { 0 };
+//	return 0;
+//}
+
+
+//#define定义的标识符常量
+//#define MAX 10
+//int main()
+//{
+//	int arr[MAX] = { 0 };
+//	printf("%d\n", MAX);
+//	return 0;
+//}
+
+
+//枚举常量（一一列举）（enum）
+//enum sex
+//{
+//	MALE,
+//	FAMALE,
+//	SECRET
+//};
+//int main()
+//{
+//	enum sex s = FAMALE;
+//	printf("%d\n", MALE);
+//	printf("%d\n", FAMALE);
+//	printf("%d\n", SECRET);
+//	printf("%d\n", s);
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
