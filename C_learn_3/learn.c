c3f1b78d9ff14c0bd99881d1229fb4053c51e0a8
ChancWys <458490240@qq.com>
2021-08-30 21:44:39 +0800

    start lesson 3

---
+++ C_learn_3/learn.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+
+//int Max(int a, int b)
+//{
+//	if (a>b)
+//		return a;
+//	else
+//		return b;
+//}
+//int main()
+//{
+//	int num1 = 10, num2 = 20, max = 0;
+//	max = Max(num1, num2);
+//	printf("max = %d\n", max);
+//	return 0;
+//}
+
+int main()
+{
+	
+	return 0;
+}
